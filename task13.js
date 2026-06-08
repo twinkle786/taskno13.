c68792d8ef1fb249c7508e2904ec1a8226bd9340
twinkle786 <twinklebhardwaj7082006@gmail.com>
2026-06-08 07:06:21 +0530

    commit

---
+++ task13.js
@@ -0,0 +1,10 @@
+let num1 = 5;
+let num2 = 10;
+
+if (num1 % 2 === 0 && num2 % 2 === 0) {
+    console.log("Both are even");
+} else if (num1 % 2 !== 0 && num2 % 2 !== 0) {
+    console.log("Both are odd");
+} else {
+    console.log("One is even and one is odd");
+}
